4c6403fc6f8c9c09eb62b467a09abc5d5dce25c0
Frantz-Sebastien Mathias <frantz-sebastienmathias@pursuit.org>
2024-12-16 23:19:16 -0500

    still working on the form

--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -1,24 +1,32 @@
 import React from 'react'
+import { useForm } from "react-hook-form"
 
 const ExpenseForm = () => {
+
+    const { register, handleSubmit, formState: { errors } } = useForm()
+
+
+
   return (
-    <form action="" className='mb-3'>
+    <form action="" className='mb-5' onSubmit={handleSubmit(data => console.log(data))}>
         
         <div classNam="mb-3">
             <label htmlFor='description' className='form-label'>Description</label>
-            <input id="description" type="text" className='form-control' />
+            <input id="description" type="text" className='form-control' {...register("description", {minLength: 6, required:true} )} />
+            <p>This field is required</p>
+            <p>This field requires at least 6 characters</p>
         </div>
 
         <div classNam="mb-3">
             <label htmlFor='amount' className='form-label'>Amount</label>
-            <input id="amount" type="text" className='form-control' />
+            <input id="amount" type="number" className='form-control' {...register("amount", {required: true})}/>
         </div>
 
         <div classNam="mb-3">
             <label htmlFor='description' className='form-label'>
                 Category
             </label>
-            <select name="" id="" className='form-select'>
+            <select name="" id="" className='form-select' {...register("category", {required: true})}>
                 <option value=""></option>
                 <option value="utilities">Utilities</option>
                 <option value="entertainment">Entertainment</option>
